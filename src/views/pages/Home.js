5bc7f89b2891d4c9ac58bf243e1215f3c640c273
Cristian Saenz <cristianmsaenza@gmail.com>
2022-04-15 18:47:42 -0500

    Fix project1

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -121,7 +121,7 @@ const homeView = () => {
                   <div class="section__proyect__wrapper">
                     <div class="project__name">
                       <div class="proyect__number title">001</div>
-                      <h3 class="project__label title__inner">Mi_Portfolio</h3>
+                      <h3 class="project__label title__inner">Portfolio</h3>
                     </div>
                     <div class="project__do">
                       <h4 class="project__do__item paragraph__inner">
